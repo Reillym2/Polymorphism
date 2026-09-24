801f0481ea7a469bedb356bc54735eaf9604338f
Reilly Martinez <martinezguerrr1021@students.bowiestate.edu>
2026-09-24 10:37:24 -0400

    Second Commit

--- src/Instructor.java
+++ src/Instructor.java
@@ -1,2 +1,15 @@
-public class Instructor {
+import java.lang.classfile.instruction.ReturnInstruction;
+
+public class Instructor extends BSU_Member{
+    String department;
+
+    Instructor(){
+        this.department = "CS";
+        this.status = "Faculty";
+    }
+
+    // Task: Create a display method that will print the department and status
+    public void Faculty (){
+        System.out.println("Inside Instructor------Department: " + this.department + "Faculty: " + this.status);
+    }
 }
